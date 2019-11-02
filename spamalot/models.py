0994a3f9bfef72dcdb8adcde77ccc87d0e57c188
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-01 20:52:01 -0400

    migrate

--- spamalot/models.py
+++ spamalot/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Spam(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    birthday = models.DateField(auto_now=False)
+    email = models.EmailField(max_length=204)
+
+    def __str__(self):
+        return self.first_name
